88f030a7b2b8962989560093eb6f8e2b7454ae37
harsha_sai <harsha.sai2813@gmail.com>
2025-08-03 12:13:54 +0530

    folder structure created

---
+++ template.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+projectName = "kidney_classifier"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{projectName}/__init__.py",
+    f"src/{projectName}/components/__init__.py",
+    f"src/{projectName}/utils/__init__.py",
+    f"src/{projectName}/config/__init__.py",
+    f"src/{projectName}/config/configuration.py",
+    f"src/{projectName}/pipeline/__init__.py",
+    f"src/{projectName}/entity/__init__.py",
+    f"src/{projectName}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+
+
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"{filedir} directory created, for the file -> {filename}")
+
+    if (not os.path.exists(filepath) or os.path.getsize(filepath)==0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"{filename} created.")
+    
+    else:
+        logging.info(f"{filepath} already exists.")
+        
